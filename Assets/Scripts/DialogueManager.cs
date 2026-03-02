2b3e9238690498794c7c05d0bc8c9860a034c0f2
optimistic-Pig <miles1017@gmail.com>
2026-03-02 14:25:27 -0500

    Added documentation

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    //initalizes variables
     public float martianOpinion = 0.0f;
     public float dialogueTracker = 0.0f;
 
@@ -11,16 +12,34 @@ public class DialogueManager : MonoBehaviour
     public TextMeshPro neutral;
     public TextMeshPro negative;
     
+    //used to increment to the next dialouge option
     public void nextLine (float opinion)
     {
         martianOpinion += opinion;
         dialogueTracker += 1.0f;
 
-        if(dialogueTracker == 1)
+        dialogueSetter(dialogueTracker);
+    }
+    //used for a specific dialouge option
+    public void setLine (float opinion, float tracker)
+    {
+        martianOpinion += opinion;
+        dialogueTracker = tracker;
+
+        dialogueSetter(dialogueTracker);
+    }
+    //is in charge of all dialouge and sets the text to what is needed
+    private void dialogueSetter(float tracker)
+    {
+        if(tracker == 1)
         {
             positive.text = "positive example 2";
             neutral.text = "neutral example 2";
             negative.text = "negative example 2";
+        }
+        else if (tracker == 2)
+        {
+
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    //Initialzies variablews
     public DialogueManager dialogueManager;
     public Camera playerCamera;
     public Camera interviewCamera;
@@ -24,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     private bool canMove = true;
     private bool onOfficeCam = true;
 
+    //this runs as soon as the game scene is laoded
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -31,18 +33,21 @@ public class PlayerMovement : MonoBehaviour
         Cursor.visible = false;
         interviewCamera.enabled = false;
     }
-
+    //this is called EVERY frame
     void Update()
     {
+        //this sets the direction which is used in the movement part
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-
+        //checks if the player is sprinting
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        //Checks if the player can move, then checks if they are running if they are use run speed if not use walk speed
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
+        //This moves based on the last code for curent speed on the x and y axis
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
+        //if the jump button is pressed then moves the player up by the jump power
         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
             moveDirection.y = jumpPower;
@@ -51,19 +56,21 @@ public class PlayerMovement : MonoBehaviour
         {
             moveDirection.y = movementDirectionY;
         }
-
+        //if in the air makes sure gravity will bring the player down
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
-        
+        //if the player left clicks
         if(Input.GetMouseButtonDown(0))
         {
             Ray ray;
+            //checks which camera is on, if the office camera shoot a raycast from the player camera
             if(onOfficeCam)
             {
                 ray = playerCamera.ScreenPointToRay(Input.mousePosition);
             }
+            //if from the interview shoots a ray from that camera
             else
             {
                 ray = interviewCamera.ScreenPointToRay(Input.mousePosition);
@@ -71,8 +78,10 @@ public class PlayerMovement : MonoBehaviour
             
             RaycastHit hit;
             float maxDistance = 100f;
+            //this part registers what happens from the ray that was cast
             if (Physics.Raycast(ray, out hit, maxDistance))
             {
+                //if the ray hits something with the interview tag it swithces the camera and allows the mouse to move but stops the player from moving
                 if (hit.collider.CompareTag("Interview"))
                 {
 Debug.Log("Did Hit, interview");
@@ -83,6 +92,7 @@ Debug.Log("Did Hit, interview");
                     interviewCamera.enabled = true;
                     onOfficeCam = false;
                 }
+                //If hit something with the office tag does the opposite, hides mouse but allows player to move, also swithces camera
                 else if (hit.collider.CompareTag("Office"))
                 {
 Debug.Log("Did Hit, office");
@@ -93,21 +103,23 @@ Debug.Log("Did Hit, office");
                     interviewCamera.enabled = false;
                     onOfficeCam = true;
                 }
+                //these options will let the dialouge manager know there is a new option and that script will dictate what dialogue to choose from
                 else if (hit.collider.CompareTag("Positive"))
                 {
                     dialogueManager.nextLine(1.0f);
                 }
-            }
-            /*ray = interviewCamera.ScreenPointToRay(Input.mousePosition);
-            
-            if (Physics.Raycast(ray, out hit, maxDistance))
-            {
-                
-                
-            }
-            */
-        }
+                else if (hit.collider.CompareTag("Neutral"))
+                {
+                    dialogueManager.nextLine(1.0f);
+                }
+                else if (hit.collider.CompareTag("Negative"))
+                {
+                    dialogueManager.nextLine(1.0f);
+                }
 
+            }
+        }
+        //Crouches if the player presses left control
         if (Input.GetKey(KeyCode.LeftControl) && canMove)
         {
             characterController.height = crouchHeight;
@@ -121,9 +133,9 @@ Debug.Log("Did Hit, office");
             walkSpeed = 6f;
             runSpeed = 12f;
         }
-
+        //moves the player
         characterController.Move(moveDirection * Time.deltaTime);
-
+        //moves camera within the player
         if (canMove)
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
